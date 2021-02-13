29b0cb0387aa8d1e2e7276297ef0cf18e880f9b9
Rob Signorelli <rob.signorelli@gmail.com>
2021-02-13 10:33:04 -0500

    Only 1 way to middleware now.

--- rpc/middleware.go
+++ rpc/middleware.go
@@ -6,28 +6,13 @@ import (
 
 /* ----- SERVER MIDDLEWARE ----- */
 
-// WithMiddleware invokes this chain of work before executing the actual HTTP handler for
-// your service call.
-func WithMiddleware(mw ...Middleware) GatewayOption {
+// WithMiddleware invokes this chain of work before executing the actual HTTP handler for your service call. Any
+// functions yu supply here will fire before your service function, but after built-in middleware functions such
+// as the one that restores metadata (i.e. your middleware will have access to request/context metadata).
+func WithMiddleware(mw ...MiddlewareFunc) GatewayOption {
 	return func(gw *Gateway) {
 		gw.middleware = mw
 	}
-}
-
-// WithMiddlewareFunc invokes this chain of work before executing the actual HTTP handler for
-// your service call.
-func WithMiddlewareFunc(funcs ...MiddlewareFunc) GatewayOption {
-	mw := make(middlewarePipeline, len(funcs))
-	for i, fn := range funcs {
-		mw[i] = fn
-	}
-	return WithMiddleware(mw...)
-}
-
-// Middleware is a component that conforms to the 'negroni' middleware handler. It accepts the
-// standard HTTP inputs as well as the rest of the computation.
-type Middleware interface {
-	ServeHTTP(w http.ResponseWriter, req *http.Request, next http.HandlerFunc)
 }
 
 // MiddlewareFunc is a component that conforms to the 'negroni' middleware function. It accepts the
@@ -42,7 +27,7 @@ func (mw MiddlewareFunc) ServeHTTP(w http.ResponseWriter, req *http.Request, nex
 
 // middlewarePipeline is a chain of middleware handlers that fire in succession before ultimately
 // executing the "real" HTTP handler that does the real work for the endpoint.
-type middlewarePipeline []Middleware
+type middlewarePipeline []MiddlewareFunc
 
 // Then wraps all of the middleware handlers capped off with the "real work" handler into a single
 // handler function that can be used by a standard net/http server.
@@ -51,7 +36,7 @@ func (pipeline middlewarePipeline) Then(handler http.HandlerFunc) http.HandlerFu
 		mw := pipeline[i]
 		next := handler
 		handler = func(res http.ResponseWriter, req *http.Request) {
-			mw.ServeHTTP(res, req, next)
+			mw(res, req, next)
 		}
 	}
 	return handler
@@ -61,7 +46,7 @@ func (pipeline middlewarePipeline) Then(handler http.HandlerFunc) http.HandlerFu
 
 // WithClientMiddleware sets the chain of HTTP request/response handlers you want to invoke
 // on each service function invocation before/after we dispatch the HTTP request.
-func WithClientMiddleware(funcs ...ClientMiddleware) ClientOption {
+func WithClientMiddleware(funcs ...ClientMiddlewareFunc) ClientOption {
 	return func(client *Client) {
 		client.middleware = funcs
 	}
@@ -70,14 +55,14 @@ func WithClientMiddleware(funcs ...ClientMiddleware) ClientOption {
 // RoundTripperFunc matches the signature of the standard http.RoundTripper interface.
 type RoundTripperFunc func(r *http.Request) (*http.Response, error)
 
-// ClientMiddleware is a round-tripper-like function that accepts a request and returns a response/error
+// ClientMiddlewareFunc is a round-tripper-like function that accepts a request and returns a response/error
 // combo, but also accepts 'next' (the rest of the computation) so that you can short circuit the
 // execution as you see fit.
-type ClientMiddleware func(request *http.Request, next RoundTripperFunc) (*http.Response, error)
+type ClientMiddlewareFunc func(request *http.Request, next RoundTripperFunc) (*http.Response, error)
 
 // clientMiddlewarePipeline is an ordered chain of client middleware handlers that should fire
 // one after another.
-type clientMiddlewarePipeline []ClientMiddleware
+type clientMiddlewarePipeline []ClientMiddlewareFunc
 
 func (pipeline clientMiddlewarePipeline) Then(handler RoundTripperFunc) RoundTripperFunc {
 	for i := len(pipeline) - 1; i >= 0; i-- {
