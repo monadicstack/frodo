3bbf8c713e26d7cbaea699f8864dec1f6fc9504e
Rob Signorelli <rob.signorelli@gmail.com>
2021-04-07 14:35:29 -0400

    Tests for generated client/gateway (#43)

---
+++ example/basic/calc/calculator_service_handler.go
@@ -0,0 +1,29 @@
+package calc
+
+import (
+	"context"
+
+	"github.com/monadicstack/frodo/rpc/errors"
+)
+
+// CalculatorServiceHandler implements all of the "real" functionality for the CalculatorService. The
+// errors that Add/Sub return are completely contrived and don't make sense for real business logic. They
+// are only included to show how you can return specific types of 4XX errors in a readable/maintainable fashion.
+type CalculatorServiceHandler struct{}
+
+func (c CalculatorServiceHandler) Add(_ context.Context, req *AddRequest) (*AddResponse, error) {
+	sum := req.A + req.B
+	if sum == 12345 {
+		// https://www.youtube.com/watch?v=li9Qf-nQgWE
+		return nil, errors.PermissionDenied("idiots denied: that's what an idiot would have on their luggage")
+	}
+	return &AddResponse{Result: sum}, nil
+}
+
+func (c CalculatorServiceHandler) Sub(_ context.Context, req *SubRequest) (*SubResponse, error) {
+	if req.A < req.B {
+		return nil, errors.BadRequest("calculator service does not support negative numbers")
+	}
+	diff := req.A - req.B
+	return &SubResponse{Result: diff}, nil
+}
